94192f2c2ac471dc9344f3c597cc91a83cada999
Maria T. Andersen <mteilmann93@gmail.com>
2022-01-16 23:21:44 +0100

    add style to buttons

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -19,5 +19,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  button {
+    height: 30px;
+    width: 150px;
+    margin: 10px 5px 10px 5px;
+    background-color: hsl(200, 50%, 70%);
+    border: transparent;
+    border-radius: 5px 5px 5px 5px;
 
+  }
 </style>
