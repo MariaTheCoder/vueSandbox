6d1bc03dd781434c2a8d8f2e7fdb5beb7955884a
Maria T. Andersen <mteilmann93@gmail.com>
2022-01-17 18:32:56 +0100

    rename class name of custom button components

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,31 +1,30 @@
 <template>
-  <div class="hello" >
+  <div class="btns">
     <button>{{ buttonText }}</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Button',
+  name: "Button",
   props: {
     buttonText: String,
-    users: { 
-      type: Array, 
-      required: false
+    users: {
+      type: Array,
+      required: false,
     },
-  }
-}
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  button {
-    height: 30px;
-    width: 150px;
-    margin: 10px 5px 10px 5px;
-    background-color: hsl(200, 50%, 70%);
-    border: transparent;
-    border-radius: 5px 5px 5px 5px;
-
-  }
+button {
+  height: 30px;
+  width: 150px;
+  margin: 10px 5px 10px 5px;
+  background-color: hsl(200, 50%, 70%);
+  border: transparent;
+  border-radius: 5px 5px 5px 5px;
+}
 </style>
